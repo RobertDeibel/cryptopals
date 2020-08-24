59e13cd3604aca3bb53f2e77ef20c6fc0f6e6797
Robert Deibel <deibel.robert@googlemail.com>
2020-08-24 17:33:37 +0200

    finish challange 1

--- set1/hex-to-base64.go
+++ set1/hex-to-base64.go
@@ -1,44 +1,23 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
-	"log"
-	"os"
 )
 
-type argumentNError uint
+// HexToBase64 converts a Hex string to a Base64 byte array and string
+// returns the base64 byte array and string, on error returns a non nil error and
+// the zero-values of the byte array and string
+func HexToBase64(encodedHex string) ([]byte, string, error) {
 
-var target []byte = []byte("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
-
-func (e argumentNError) Error() string {
-	return fmt.Sprintf("Error: wrong number of arguments: %v", uint(e))
-}
-
-func main() {
-	args := os.Args[:]
-
-	if len(args) != 2 {
-		log.Fatal(argumentNError(len(args)))
-		return
-	}
-	hexDec, err := hex.DecodeString(args[1])
+	hexDec, err := hex.DecodeString(encodedHex)
 	if err != nil {
-		log.Fatal(err)
+		return nil, "", err
 	}
-	fmt.Printf("len hexDec: %d len string: %d len target: %d\n", len(hexDec), len(args[1]), len(target))
-	fmt.Printf("hex1: %v\n", hexDec)
-	var base64 []byte
-	for i := 0; i < (len(hexDec)-1)/3; i++ {
-		j := i * 3
-		k := i * 4
-		fourByte := hexDec[j : j+3]
-		base64[k] = (fourByte[0] & (128 + 64 + 32 + 16 + 8 + 4)) >> 2
-		base64[k+1] = ((fourByte[0] & (2 + 1)) << 4) + ((fourByte[1] & (128 + 64 + 32 + 16)) >> 4)
-		base64[k+2] = ((fourByte[1] & (8 + 4 + 2 + 1)) << 2) + ((fourByte[2] & (128 + 64)) >> 6)
-		base64[k+3] = (fourByte[2] & (32 + 16 + 8 + 4 + 2 + 1))
-	}
-	fmt.Printf("base64Dec: %v", base64)
-
-	fmt.Println("Hex stream: ")
+	base64Dec := make([]byte, base64.StdEncoding.EncodedLen(len(hexDec)))
+	fmt.Println(hexDec)
+	base64.StdEncoding.Encode(base64Dec, hexDec)
+	base64Str := string(base64Dec)
+	return base64Dec, base64Str, nil
 }
